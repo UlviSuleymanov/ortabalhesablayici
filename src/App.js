d07b94bd11d42860e5685b6af67b7d7d50c43dca
Ülvi Süleymanov <100337078+UlviSuleymanov@users.noreply.github.com>
2022-10-04 17:51:53 +0400

    created functionality

--- src/App.js
+++ src/App.js
@@ -2,32 +2,41 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [result, setResult] = useState(0);
+  const [result, setResult] = useState('');
   const [bal1, setBal1] = useState(0);
   const [bal2, setBal2] = useState(0);
   const [bal3, setBal3] = useState(0);
-
   const [kredit1, setKredit1] = useState(0);
   const [kredit2, setKredit2] = useState(0);
   const [kredit3, setKredit3] = useState(0);
-
+  const bals = parseInt(bal1 * kredit1 + bal2 * kredit2 + bal3 * kredit3);
+  const kredits = parseInt(kredit1 + kredit2 + kredit3);
   const calculate = () => {
-    const bals = parseInt(bal1 * kredit1 + bal2 * kredit2 + bal3 * kredit3);
-    const kredits = parseInt(kredit1 + kredit2 + kredit3);
-    console.log(bals);
-    console.log(kredits);
+    if (bals === 0 && kredits === 0) {
+      return setResult('Reqem Girniz');
+    }
     return setResult(bals / kredits);
+  };
+  const remove = () => {
+    if (result > 1) {
+    }
   };
 
   return (
     <div className="App">
       <h1>Orta Bal Hesablayıcı</h1>
-      {result ? <h3>{result}</h3> : ''}
+      {isNaN(result) && bals !== 0 && kredits !== 0 ? <h3>Reqemden istifade edin</h3> : ''}
+      {result === 'Reqem Girniz' ? <h3>{result}</h3> : ''}
+      {result > 0 ? <h3>Sizin baliniz : {result}</h3> : ''}
       <div>
         <input
           type="text"
           onChange={(event) => {
-            setBal1(parseInt(event.target.value));
+            if (event.target.value !== isNaN) {
+              setBal1(parseInt(event.target.value));
+            } else {
+              alert('Duzgun  qiymet yaz');
+            }
           }}
           placeholder="Bal"
         />
@@ -72,6 +81,7 @@ function App() {
         />
       </div>
       <button onClick={() => calculate()}>Hesabla</button>
+      <button onClick={() => remove()}>Yeniləmək</button>
     </div>
   );
 }
